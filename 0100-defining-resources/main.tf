126aebfff38fb20b68b3d943c5f3b3554989bf16
Raju <raju.gandhi@gmail.com>
2020-06-14 15:43:42 -0400

    feat: introduce exercise on basic HCL syntax/creating ec2 instance

---
+++ 0100-defining-resources/main.tf
@@ -0,0 +1,16 @@
+provider "aws" {
+  access_key = "<INSERT_YOUR_AWS_ACCESS_KEY_HERE>"
+  secret_key = "<INSERT_YOUR_AWS_SECRET_KEY_HERE>"
+  region     = "us-east-1"
+}
+
+# TODO
+# Define an EC2 instance (aws_instance) with the following constraints:
+# Resource identifier - exercise_0010
+#
+# ami = ami-07ebfd5b3428b6f4d
+# instance type = t2.micro
+#
+# Be sure to tag it with:
+# - "Name" to "exercise_0010"
+# - "Terraform" to true
