5de59deb5d2ec36611833c786e48eed88b3a8bec
Raju <raju.gandhi@gmail.com>
2020-06-14 16:40:38 -0400

    feat: introduce exercise on using outputs

---
+++ 0300-output/main.tf
@@ -0,0 +1,48 @@
+########################
+# 𝖴𝖲𝖤𝖲 𝖣𝖤𝖥𝖠𝖴𝖫𝖳 𝖵𝖯𝖢 !!!
+########################
+resource "aws_security_group" "sec_ssh" {
+  # you can have as many ingress/egress blocks as you want
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name      = "sec_ssh"
+    Terraform = true
+  }
+}
+
+# Demo
+output sec_ssh {
+  value = aws_security_group.sec_ssh.arn
+}
+
+resource "aws_instance" "exercise_0030" {
+  ami           = "ami-085925f297f89fce1"
+  instance_type = "t2.micro"
+
+  vpc_security_group_ids = [
+    aws_security_group.sec_ssh.id,
+  ]
+
+  tags = {
+    Name      = "exercise_0030"
+    Terraform = true
+  }
+}
+
+# TODO
+# Output the public IP address of the exercise_0030 instance.
+# Read the docs at https://www.terraform.io/docs/providers/aws/r/instance.html#attributes-reference
+# to see what attribute you are going to use to look up
---
+++ 0300-output/provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "<INSERT_YOUR_AWS_ACCESS_KEY_HERE>"
+  secret_key = "<INSERT_YOUR_AWS_SECRET_KEY_HERE>"
+  region     = "us-east-1"
+}
