4df28beef88a6e0e5270fd0b9c3b718447fdd0f1
Raju <raju.gandhi@gmail.com>
2020-06-14 19:15:07 -0400

    feat: add files to demo remote state using S3

--- 0700-remote-state/provider.tf
+++ 0700-remote-state/provider.tf
@@ -1,5 +1,7 @@
 provider "aws" {
-  region = "us-east-1"
+  access_key = "YOUR_AWS_KEY_ID_HERE"
+  secret_key = "YOUR_SECRET_ACCESS_KEY_HERE"
+  region     = "us-east-1"
 }
 
 resource "aws_s3_bucket" "my_terraform_state" {
