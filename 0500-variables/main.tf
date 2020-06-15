c92fa8db97b01e0c18ef339dd6da171f4f6a2dcd
Raju <raju.gandhi@gmail.com>
2020-06-14 20:01:04 -0400

    feat: solution for exercise on using variables and functions

--- 0500-variables/main.tf
+++ 0500-variables/main.tf
@@ -25,4 +25,13 @@ data "aws_ami" "latest_ubuntu" {
 # - "Terraform" to true
 #
 # Add a THIRD tag called `env` that uses the value of the `tier` variable
+resource "aws_instance" "exercise_0050" {
+  ami           = data.aws_ami.latest_ubuntu.id
+  instance_type = "t2.micro"
 
+  tags = {
+    Name      = "${upper(var.tier)}-example"
+    Terraform = true
+    env       = var.tier
+  }
+}
--- 0500-variables/variables.tf
+++ 0500-variables/variables.tf
@@ -15,4 +15,7 @@ variable "AWS_DEFAULT_REGION" {
 # define a new variable called 'tier' of type 'string' to specify the env
 # and use that in your EC2 instance.
 #  Default it to 'dev'
-
+variable tier {
+  type = string
+  default = "dev"
+}
