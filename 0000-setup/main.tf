c6ae69ee41e163f1b89c91d14805e2d4b8b5453a
Raju Gandhi <raju.gandhi@gmail.com>
2021-12-03 18:17:10 -0500

    feat: upgrade to TF >1.0.0

--- 0000-setup/main.tf
+++ 0000-setup/main.tf
@@ -1,15 +1,15 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.68.0"
     }
   }
 }
 
 provider "aws" {
-  region     = "us-east-1"
+  region = "us-east-1"
 }
 
 resource "aws_instance" "exercise_0000" {
--- 0700-remote-state/version.tf
+++ 0700-remote-state/version.tf
@@ -1,9 +1,9 @@
 terraform {
-  required_version = ">= 0.13"
+  required_version = ">= 1.0.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 3.0"
+      version = "~> 3.68.0"
     }
   }
 
